5754d472adabf2900af2cf9b0b54743e0aa2af55
arif-rahman-cse <smrahmanarif905@gmail.com>
2020-07-06 17:19:38 +0600

    travello home page added

--- test_app/views.py
+++ test_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html', {'name': 'Django Framwork'})
+    return render(request, 'index.html')
 
 def showDetails(request):
     userName = request.POST['username']
