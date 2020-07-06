a934e4a87379ac3ccb4c1c7c563d53bfd9ea92b9
arif-rahman-cse <smrahmanarif905@gmail.com>
2020-07-06 15:26:52 +0600

    New page added & data passing one page to another

--- test_app/views.py
+++ test_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'home.html', {'name': 'Django Framwork'})
+
+def showDetails(request):
+    userName = request.GET['username']
+    return render(request, 'welcome_page.html', {'email': userName})
